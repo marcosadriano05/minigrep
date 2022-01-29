5b2e077d22018ee28b8168c77c7d8966e36067da
Marcos Adriano <marcosadriano740@gmail.com>
2022-01-29 20:41:48 -0300

    feat: error handling to not enought arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Searching fro {}", config.query);
     println!("In file {}", config.filename);
     let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
@@ -16,9 +20,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enought arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
